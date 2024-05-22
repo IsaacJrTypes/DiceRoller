0815562a05fffeab9b01572572df42960453d6f7
Isaac J <jimenez.isaac.jr@gmail.com>
2024-05-21 23:41:54 -0700

    add dice game

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,14 +15,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -38,13 +35,24 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+fun compareDices(dice1: Int, dice2: Int): String {
+    return when {
+        dice1 > dice2 -> "Dice 1 wins"
+        dice1 < dice2 -> "Dice 2 wins"
+        else -> "Tie"
+    }
+}
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier
-    .fillMaxSize()
-    .wrapContentSize(Alignment.Center)) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
+    var result1 by remember { mutableStateOf(1) }
+    var result2 by remember { mutableStateOf(1) }
+
+    val imageResource1 = when (result1) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -52,20 +60,49 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
+    val imageResource2 = when (result2) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    val msg = compareDices(result1, result2)
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = result.toString()
+        Text(
+            text = msg,
+            fontSize = 52.sp,
+            fontWeight = FontWeight.Bold
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(stringResource(R.string.roll))
+
+        Image(
+            painter = painterResource(imageResource1),
+            contentDescription = result1.toString()
+        )
+        Button(onClick = { result1 = (1..6).random() }) {
+
+            Text(stringResource(R.string.roll1))
+        }
+        Button(onClick = { result2 = (1..6).random() }) {
+            Text(stringResource(R.string.roll2))
+        }
+        Image(
+            painter = painterResource(imageResource2),
+            contentDescription = result2.toString()
+        )
+        Button(onClick = {
+            result1 = 1
+            result2 = 1
+        }) {
+            Text("Reset")
         }
     }
 }
+
 @Preview
 @Composable
 fun DiceRollerApp() {
